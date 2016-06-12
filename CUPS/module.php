98eb87fcd27cd62c5e9e672b1fe5ca624e86f045
sallos <lars@mordenham.de>
2016-06-12 21:49:47 +0200

    Update

--- CUPS/module.php
+++ CUPS/module.php
@@ -5,6 +5,7 @@
 
         var $IP_C;
         var $IP_D;
+        var $NAME;
 
                 public function Create()
                 {
@@ -15,6 +16,7 @@
                         //You cannot use variables here. Just static values.
                         $this->RegisterPropertyString("CURL_IP", "127.0.0.1");
                         $this->RegisterPropertyString("DRUCKER_IP", "127.0.0.1");
+                        $this->RegisterPropertyString("DRUCKER_NA", "Test_Drucker");
        
                 }
 
@@ -26,13 +28,37 @@
                         
                         $this->IP_C = $this->ReadPropertyString("CURL_IP");
                         $this->IP_D = $this->ReadPropertyString("DRUCKER_IP");
+                        $this->NAME = $this->ReadPropertyString("DRUCKER_NA");
+
+                        $this->RegisterVariableBoolean("Drucker_ON", "Drucker Online");
+                        $this->RegisterVariableInteger("Offene_AUF", "Offene_Aufträge");
                       
 			
 		}
-		
 
-		
+                private function GetCUPS()
+                {
+                        $this->IP_C = $this->ReadPropertyString("CURL_IP");
+                        $this->IP_D = $this->ReadPropertyString("DRUCKER_IP");
+                        $this->NAME = $this->ReadPropertyString("DRUCKER_NA");
+
+                        $URL = "http://" . $IP_C . ":631/printers/" . $NAME;
+
+                        $BUFFER = implode('', file($URL));
+    
+                        $Anfang = strpos($BUFFER, "Showing");
+                        $Ende = strpos($BUFFER, "active job");
+                        $Lang = $Ende - $Anfang;
+
+                        $TEMP = substr($BUFFER, $Anfang, $Lang);
+                        $DUMP = strtok($TEMP, " ");
+                        $ERG = intval(strtok(" "));
+
+                        SetValue($this->GetIDForIdent("Offene_AUF"), $ERG);
+
 	
-	}
+                }
+
+        }
 
 ?>
